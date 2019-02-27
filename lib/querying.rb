ff205ba7f264e86f6638f848163cbae8b194ca40
ktochow1 <ktochow1@binghamton.edu>
2019-02-27 18:31:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title FROM Series INNER JOIN Characters ON Series.id = Characters.series_id COUNT(Characters.speices) FROM Characters GROUP BY species WHERE species = 'human';"
+  "SELECT Series.title FROM Series INNER JOIN Characters ON Series.id = Characters.series_id GROUP BY species WHERE species = 'human' COUNT(Characters.speices) FROM Characters;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
