f9c0042f32984536774e1866109c1272558998b0
ktochow1 <ktochow1@binghamton.edu>
2019-02-27 18:00:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,8 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT Authors.name, Subgenres.name FROM Authors INNER JOIN Subgenres ON Authors.id = Subgenres.id 
-  INNER JOIN Series ON Authors.id = Series.id;"
+  "SELECT Authors.name, Subgenres.name FROM Authors INNER JOIN Subgenres ON Authors.id = Subgenres.id INNER JOIN Series ON Authors.id = Series.id;"
 end
 
 def select_series_title_with_most_human_characters
